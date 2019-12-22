71f5738b5f65ef67835ff5b518429e88a41ad543
Alvaro E F Mota <alvaro11318@gmail.com>
2019-12-21 23:34:28 -0300

    Criaçao de variaveis, cast e manipulaçao de string

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
-<?php
-	echo("ta funcionando");
-	echo("ta funcionando");
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+
+<body>
+	<?php
+	$ano = "1996asdasdasdad";
+	$ano = (string)(int)$ano;
+	$ano .= "#";
+	$ano = $ano."@";	
+	$nome = "Alvaro Ernani Fonseca Mota";
+	$idade = 23;
+	echo $nome. " tem ". $idade. " anos e nasceu no ano de ". $ano;
+	?>
+</body>
+
+</html>
